aebff69e3943e9962f9248425b20173bc083e320
Rémi Becheras <rbecheras@sirap.fr>
2016-05-31 18:59:27 +0200

    create menu list of available formats

--- src/classes/DynamicdatePlugin.js
+++ src/classes/DynamicdatePlugin.js
@@ -1,14 +1,27 @@
 'use strict'
 
+var DateTimeMenuItem = require('./DateTimeMenuItem')
+
 module.exports = DynamicdatePlugin
 
 function DynamicdatePlugin (editor) {
+  // var now = new Date()
   editor.addMenuItem('dynamicdate', {
     context: 'insert',
     text: 'Dynamic date',
     type: 'menuitem',
     menu: [
-      { text: 'sss' }
+      new DateTimeMenuItem('dd/mm/yyyy, HH\'h\'MM\'\'\'ss'),
+      new DateTimeMenuItem('dd/mm/yyyy, HH\'h\'MM'),
+      new DateTimeMenuItem('dd/mm/yyyy'),
+      new DateTimeMenuItem('HH\'h\'MM\'\'\'ss'),
+      new DateTimeMenuItem('HH\'h\'MM'),
+      new DateTimeMenuItem('dddd'),
+      new DateTimeMenuItem('dd'),
+      new DateTimeMenuItem('mm'),
+      new DateTimeMenuItem('mmmm'),
+      new DateTimeMenuItem('yy'),
+      new DateTimeMenuItem('yyyy')
     ]
   })
 }
