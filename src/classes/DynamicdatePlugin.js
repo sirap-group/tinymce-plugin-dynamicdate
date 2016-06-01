3bcb8dd2c7e69811e228cddab07a8b67192fd4fa
Rémi Becheras <rbecheras@sirap.fr>
2016-06-01 10:40:08 +0200

    create createDateTimeMenuItemList inner function

--- src/classes/DynamicdatePlugin.js
+++ src/classes/DynamicdatePlugin.js
@@ -6,19 +6,7 @@ var DateTimeMenuItem = require('./DateTimeMenuItem')
 module.exports = DynamicdatePlugin
 
 function DynamicdatePlugin (editor) {
-  var dateTimeMenuItemList = [
-    new DateTimeMenuItem('dd/mm/yyyy, HH\'h\'MM\'\'\'ss'),
-    new DateTimeMenuItem('dd/mm/yyyy, HH\'h\'MM'),
-    new DateTimeMenuItem('dd/mm/yyyy'),
-    new DateTimeMenuItem('HH\'h\'MM\'\'\'ss'),
-    new DateTimeMenuItem('HH\'h\'MM'),
-    new DateTimeMenuItem('dddd'),
-    new DateTimeMenuItem('dd'),
-    new DateTimeMenuItem('mm'),
-    new DateTimeMenuItem('mmmm'),
-    new DateTimeMenuItem('yy'),
-    new DateTimeMenuItem('yyyy')
-  ]
+  var dateTimeMenuItemList = createDateTimeMenuItemList()
 
   editor.addMenuItem('dynamicdate', {
     context: 'insert',
@@ -38,4 +26,20 @@ function DynamicdatePlugin (editor) {
       })
     }
   })
+}
+
+function createDateTimeMenuItemList () {
+  return [
+    new DateTimeMenuItem('dd/mm/yyyy, HH\'h\'MM\'\'\'ss'),
+    new DateTimeMenuItem('dd/mm/yyyy, HH\'h\'MM'),
+    new DateTimeMenuItem('dd/mm/yyyy'),
+    new DateTimeMenuItem('HH\'h\'MM\'\'\'ss'),
+    new DateTimeMenuItem('HH\'h\'MM'),
+    new DateTimeMenuItem('dddd'),
+    new DateTimeMenuItem('dd'),
+    new DateTimeMenuItem('mm'),
+    new DateTimeMenuItem('mmmm'),
+    new DateTimeMenuItem('yy'),
+    new DateTimeMenuItem('yyyy')
+  ]
 }
